04b23a542f672d9ab8b01671b03d4ddd40c14408
Phil Assheton <philip.assheton@insone.de>
2025-07-14 10:54:33 +0200

    Major likelihood ratio rewrite:
    (i) Move BFGS into separate file (and separate step)
    (ii) Allow to experiment with unrolling Adam loop or not
    (iii) Allow setting hyperparameters of Adam loop
    (iv) Directly clip tanh values rather than *0.99
    (v) Penalise likelihood outside of those bounds

--- examples/biserial_partial_correlation/biparcorr_likelihood_ratio_funcs.py
+++ examples/biserial_partial_correlation/biparcorr_likelihood_ratio_funcs.py
@@ -2,15 +2,26 @@ import biparcorr_analyse_funcs as biparcorr
 
 import tensorflow as tf
 import tensorflow_probability as tfp
-tfd, tfb = tfp.distributions, tfp.bijectors
 
 import functools
 
 # typing
-from typing import Optional, Union, Tuple
+from typing import Optional, Union, Tuple, Dict
 from neuralcis.common import Batch, Samples, Stats, UnknownParams, Ys
 from tensor_annotations.tensorflow import Tensor0, Tensor1, Tensor2, Tensor3
 from tensor_annotations.tensorflow import float32 as tf32, int64 as ti64
+
+
+MAX_ABS_RHO = 0.99
+HYPERPARAMETERS_DEFAULT = {
+    'adam_iterations': tf.constant(120, dtype=tf.int64),
+    'adam_polish_iterations': tf.constant(40, dtype=tf.int64),
+    'learning_rate_multiplier': tf.constant(1.0, dtype=tf.float32),
+    'learning_rate_polish_decay': tf.constant(0.9, dtype=tf.float32),
+    'beta1': tf.constant(0.8, dtype=tf.float32),
+    'beta2': tf.constant(0.99, dtype=tf.float32),
+}
+UNROLL_WHEN_COMPILING = False  # Faster but much slower to compile
 
 
 def log_likelihood(
@@ -37,7 +48,7 @@ def log_likelihood(
         tf.stack([one, rho_bc], axis=-1),
         tf.stack([rho_bc, one], axis=-1),
     ], axis=-1)
-    mvn_bc = tfd.MultivariateNormalTriL(
+    mvn_bc = tfp.distributions.MultivariateNormalTriL(
         loc=tf.zeros(2),
         scale_tril=tf.linalg.cholesky(cov_bc)[:, None, :, :],
     )
@@ -63,22 +74,39 @@ def log_likelihood(
     log_prob_abc_masked = biparcorr.n_mask(n) * (log_prob_a + log_prob_bc)
 
     log_likelihood_final = tf.reduce_sum(log_prob_abc_masked, axis=1)
+
     return log_likelihood_final                                                # type: ignore
+
+
+def penalise_log_likelihood(
+        log_likelihoods: Tensor1[tf32, Batch],
+        params_unknown_transformed: Tensor2[tf32, Batch, UnknownParams],
+) -> Tensor1[tf32, Batch]:
+
+    max_abs_rho_transformed = transform_correlations(tf.constant(MAX_ABS_RHO))
+    max_abs = tf.reduce_max(tf.math.abs(params_unknown_transformed), axis=1)
+    penalty_unscaled = tf.nn.relu(max_abs - max_abs_rho_transformed)
+
+    return log_likelihoods - 10.*penalty_unscaled
 
 
 def log_likelihood_from_transformed_params(
         stats_tensor: Tensor3[tf32, Batch, Samples, Stats],
         n: Tensor1[tf32, Batch],
         params_unknown_transformed: Tensor2[tf32, Batch, UnknownParams],
+        penalise_boundaries: bool = True,
 ) -> Tensor1[tf32, Batch]:
 
     rho_ab_partial, rho_bc, rho_ac, prop_a = untransform_params(
         params_unknown_transformed,
-        includes_rho_ab_partial=True,
     )
-    return log_likelihood(stats_tensor,
-                          rho_ab_partial, rho_bc, rho_ac, prop_a,
-                          n)
+
+    log_likelihoods = log_likelihood(stats_tensor,
+                                     rho_ab_partial, rho_bc, rho_ac, prop_a, n)
+    if penalise_boundaries:
+        log_likelihoods = penalise_log_likelihood(log_likelihoods,
+                                                  params_unknown_transformed)
+    return log_likelihoods
 
 
 def log_likelihood_from_transformed_params_fixed_ab(
@@ -86,37 +114,49 @@ def log_likelihood_from_transformed_params_fixed_ab(
         n: Tensor1[tf32, Batch],
         rho_ab_partial_transformed: Tensor1[tf32, Batch],
         params_unknown_transformed: Tensor2[tf32, Batch, UnknownParams],
+        penalise_boundaries: bool = True,
 ) -> Tensor1[tf32, Batch]:
 
-    rho_bc, rho_ac, prop_a = untransform_params(params_unknown_transformed,
-                                                includes_rho_ab_partial=False)
+    params_unknown_transformed = tf.concat([
+        rho_ab_partial_transformed[:, None],
+        params_unknown_transformed,
+    ], axis=1)
+    return log_likelihood_from_transformed_params(
+        stats_tensor,
+        n,
+        params_unknown_transformed,
+        penalise_boundaries,
+    )
 
-    return log_likelihood(stats_tensor,
-                          rho_ab_partial, rho_bc, rho_ac, prop_a,
-                          n)
 
-
-@tf.function(jit_compile=True)
+# Maybe a tf.function: see directly after function
 def adam(
         likelihood_function,
         params_unknown_transformed_init: Tensor2[tf32, Batch, UnknownParams],
         m: Tensor2[tf32, Batch, UnknownParams],
         v: Tensor2[tf32, Batch, UnknownParams],
         t0: Tensor0[ti64],
-        steps: int,
+        steps: Tensor0[ti64],
         lr_init: Tensor1[tf32, Batch],
         lr_decay: Union[Tensor0[tf32], Tensor1[tf32, Batch]],
+        **hyperparameters: Tensor0,
 ) -> Tuple[Tensor2[tf32, Batch, UnknownParams],
            Tensor2[tf32, Batch, UnknownParams],
            Tensor2[tf32, Batch, UnknownParams],
            Tensor1[tf32, Batch]]:
 
-    beta1 = 0.8
-    beta2 = 0.99
+    beta1 = hyperparameters['beta1']
+    beta2 = hyperparameters['beta2']
 
     lr = lr_init
     params_unknown_transformed = params_unknown_transformed_init
-    for t in range(steps):  # Deliberately a Python loop; gets unrolled.
+
+    if UNROLL_WHEN_COMPILING:
+        ts = range(int(steps))  # will create Python loop that gets unrolled.
+    else:
+        ts = tf.range(steps)  # will create a tf loop that doesn't get unrolled
+
+    for t in ts:
         with tf.GradientTape() as tape:
             tape.watch(params_unknown_transformed)
             log_likelihood_per_item = likelihood_function(
@@ -125,8 +165,9 @@ def adam(
             log_likelihood = tf.reduce_sum(log_likelihood_per_item)
         gradient = tape.gradient(log_likelihood, params_unknown_transformed)
 
-        m = beta1 * m + (1-beta1) * gradient
-        v = beta2 * v + (1-beta2) * tf.square(gradient)
+        one = tf.constant(1.0, tf.float32)
+        m = beta1 * m + (one - beta1) * gradient
+        v = beta2 * v + (one - beta2) * tf.square(gradient)
         m_hat = m / (1. - beta1**tf.cast(t0 + t + 1, tf.float32))
         v_hat = v / (1. - beta2**tf.cast(t0 + t + 1, tf.float32))
 
@@ -136,110 +177,106 @@ def adam(
     return params_unknown_transformed, m, v, log_likelihood_per_item
 
 
+if not UNROLL_WHEN_COMPILING:
+    adam = tf.function(adam, jit_compile=True)
+
+
+def transform_correlations(
+        correlations: Union[Tensor1, Tensor2],
+) -> Union[Tensor1, Tensor2]:
+
+    corrs_clipped = tf.clip_by_value(correlations, -MAX_ABS_RHO, MAX_ABS_RHO)
+    corrs_tranformed = tf.math.atanh(corrs_clipped)
+    return corrs_tranformed
+
+
+def untransform_correlations(
+    corrs_transformed: Union[Tensor1, Tensor2],
+) -> Union[Tensor1, Tensor2]:
+
+    correlations = tf.math.tanh(corrs_transformed)
+    return tf.clip_by_value(correlations, -MAX_ABS_RHO, +MAX_ABS_RHO)
+
+
 def transform_params(
-    rho_ab_partial: Optional[Tensor1[tf32, Batch]],
-    rho_bc: Tensor1[tf32, Batch],
-    rho_ac: Tensor1[tf32, Batch],
-    prop_a: Tensor1[tf32, Batch],
+    *params_list: Tensor1[tf32, Batch],
 ) -> Tensor2[tf32, Batch, UnknownParams]:
 
-    prop_a_stretched = prop_a * 2. - 1.  # -> [-1, 1] like a correlation
-    if rho_ab_partial is None:
-        params = tf.stack([
-            rho_bc,
-            rho_ac,
-            prop_a_stretched,
-        ], axis=1)
-    else:
-        params = tf.stack([
-            rho_ab_partial,
-            rho_bc,
-            rho_ac,
-            prop_a_stretched,
-        ], axis=1)
-    params_clipped = tf.clip_by_value(params, -0.98, 0.98)
-    params_transformed = tf.math.atanh(params_clipped / 0.99)
-    return params_transformed
+    prop_a = params_list[-1]
+    prop_a_stretched = prop_a * 2. - 1.
+    true_correlations_list = list(params_list[:-1])
+    correlations = tf.stack(true_correlations_list + [prop_a_stretched],
+                            axis=1)
+    return transform_correlations(correlations)
 
 
 def untransform_params(
         params_transformed: Tensor2[tf32, Batch, UnknownParams],
-        includes_rho_ab_partial: bool = True,
-) -> Union[Tuple[Tensor1[tf32, Batch],
-                 Tensor1[tf32, Batch],
-                 Tensor1[tf32, Batch],
-                 Tensor1[tf32, Batch]],
-           Tuple[Tensor1[tf32, Batch],
-                 Tensor1[tf32, Batch],
-                 Tensor1[tf32, Batch]]]:
+) -> Tuple[Tensor1[tf32, Batch], ...]:
 
-    params = tf.math.tanh(params_transformed) * 0.99
-    if includes_rho_ab_partial:
-        (rho_ab_partial,
-         rho_bc, rho_ac, prop_a_stretched) = tf.unstack(params, axis=1)
-    else:
-        rho_bc, rho_ac, prop_a_stretched = tf.unstack(params, axis=1)
-
+    params_stretched = untransform_correlations(params_transformed)
+    params_stretched = tf.unstack(params_stretched, axis=1)
+    true_correlations = params_stretched[:-1]
+    prop_a_stretched = params_stretched[-1]
     prop_a = (prop_a_stretched + 1.) / 2.
 
-    if includes_rho_ab_partial:
-        return rho_ab_partial, rho_bc, rho_ac, prop_a
-    else:
-        return rho_bc, rho_ac, prop_a
+    return tuple(true_correlations + [prop_a])
 
 
-def multistep_adam(
+def adam_and_polish(
         likelihood_function,
         params: Tensor2[tf32, Batch, UnknownParams],
         n: Tensor1[tf32, Batch],  # Just for setting learning rate.
-) -> Tensor2[tf32, Batch, Ys]:
+        **hyperparameters: Tensor0,
+) -> Tensor2[tf32, Batch, UnknownParams]:
+
+    lr_init = hyperparameters['learning_rate_multiplier'] / n
+    lr_decay = hyperparameters['learning_rate_polish_decay']
+    iterations = hyperparameters['adam_iterations']
+    iterations_polish = hyperparameters['adam_polish_iterations']
 
     m = tf.zeros_like(params)
     v = tf.zeros_like(params)
-    tf_int = lambda t0: tf.constant(t0, dtype=tf.int64)
 
-    params40, m40, v40, log_likelihood40 = adam(
+    params, m, v, _ = adam(
         likelihood_function,
-        params, m, v, tf_int(0),
-        steps=40,
+        params, m, v, tf.constant(0, dtype=tf.int64),
+        steps=iterations,
         lr_init=lr_init,
         lr_decay=tf.constant(1.0),
+        **hyperparameters,
     )
-    params80, m80, v80, log_likelihood80 = adam(
+    params, m, v, log_likelihood = adam(
         likelihood_function,
-        params40, m40, v40, tf_int(40),
-        steps=40,
+        params, m, v, iterations,
+        steps=iterations_polish,
         lr_init=lr_init,
-        lr_decay=tf.constant(1.0),
-    )
-    params120, m120, v120, log_likelihood120 = adam(
-        likelihood_function,
-        params80, m80, v80, tf_int(80),
-        steps=40,
-        lr_init=lr_init,
-        lr_decay=tf.constant(1.0),
-    )
-    _, _, _, log_likelihood120_40 = adam(
-        likelihood_function,
-        params120, m120, v120, tf_int(120),
-        steps=40,
-        lr_init=lr_init,
-        lr_decay=tf.constant(0.9),
+        lr_decay=lr_decay,
+        **hyperparameters,
     )
 
-    return tf.stack([log_likelihood40,
-                     log_likelihood80,
-                     log_likelihood120,
-                     log_likelihood120_40], axis=1)
+    return params
 
 
 def likelihood_ratios_via_gradient_ascent(
         stats_tensor: Tensor3[tf32, Batch, Samples, Stats],
         rho_ab_partial_null: Tensor1[tf32, Batch],
         rho_ab_partial_power: Tensor1[tf32, Batch],
+        rho_bc: Tensor1[tf32, Batch],
+        rho_ac: Tensor1[tf32, Batch],
+        prop_a: Tensor1[tf32, Batch],
         n: Tensor1[tf32, Batch],
+        **hyperparameters,
 ) -> Tensor2[tf32, Batch, Ys]:
 
+    params_unknown_transformed_init = transform_params(
+        rho_ab_partial_null,
+        rho_bc,
+        rho_ac,
+        prop_a,
+    )
+    rho_ab_partial_null_trans = transform_correlations(rho_ab_partial_null)
+    rho_ab_partial_power_trans = transform_correlations(rho_ab_partial_power)
 
     log_likelihood_fn_alternative = functools.partial(
         log_likelihood_from_transformed_params,
@@ -247,126 +284,52 @@ def likelihood_ratios_via_gradient_ascent(
     )
     log_likelihood_fn_null = functools.partial(
         log_likelihood_from_transformed_params_fixed_ab,
-        stats_tensor, n, rho_ab_partial_null,
+        stats_tensor, n, rho_ab_partial_null_trans,
     )
     log_likelihood_fn_power_null = functools.partial(
         log_likelihood_from_transformed_params_fixed_ab,
-        stats_tensor, n, rho_ab_partial_power,
+        stats_tensor, n, rho_ab_partial_power_trans,
     )
 
-    # use sample correlations as basis for initial guess
-    correlations_hat = biparcorr.estimate_correlations_safe(stats_tensor,
-                                                            n_float)
-    prop_a_hat = tf.reduce_sum(stats_tensor[:, :, 0], axis=1) / n_float
-
-    rho_ab_hat, rho_bc_hat, rho_ac_hat = tf.unstack(correlations_hat, axis=1)
-    rho_ab_partial_hat = ((rho_ab_hat - rho_bc_hat*rho_ac_hat) /
-                          tf.sqrt((1. - tf.square(rho_bc_hat))
-                                  * (1. - tf.square(rho_ac_hat))))
-
-    params_unknown_transformed_init = transform_params(rho_ab_partial_hat,
-                                                       rho_bc_hat,
-                                                       rho_ac_hat,
-                                                       prop_a_hat)
-
-    log_likelihoods_null = multistep_adam(
-        log_likelihood_fn_null,
-        params_unknown_transformed_init[:, 1:],
-        n_float
-    )
-    log_likelihoods_alt = multistep_adam(
+    params_alt = adam_and_polish(
         log_likelihood_fn_alternative,
         params_unknown_transformed_init,
-        n_float,
+        n,
+        **hyperparameters,
     )
-    log_likelihoods_power_null = multistep_adam(
+    params_null = adam_and_polish(
+        log_likelihood_fn_null,
+        params_alt[:, 1:],
+        n,
+        **hyperparameters,
+    )
+    params_power_null = adam_and_polish(
         log_likelihood_fn_power_null,
-        params_unknown_transformed_init[:, 1:],
-        n_float,
+        params_alt[:, 1:],
+        n,
+        **hyperparameters,
     )
 
-    diffs = tf.concat([
+    # Now we need to recover the **unpenalised** log likelihoods
+    log_likelihoods_alt = log_likelihood_fn_alternative(
+        params_alt,
+        penalise_boundaries=False,
+    )
+    log_likelihoods_null = log_likelihood_fn_null(
+        params_null,
+        penalise_boundaries=False,
+    )
+    log_likelihoods_power_null = log_likelihood_fn_power_null(
+        params_power_null,
+        penalise_boundaries=False,
+    )
+
+    diffs = tf.stack([
         log_likelihoods_alt - log_likelihoods_null,
-        log_likelihoods_alt[:, -1:] - log_likelihoods_power_null[:, -1:],
+        log_likelihoods_alt - log_likelihoods_power_null,
     ], axis=1)
 
     return diffs
-        n,
-        params_unknown_transformed,
-    )
-
-
-        n,
-        rho_ab_partial,
-        params_unknown_transformed
-    )
-
-
-def likelihood_ratio_via_line_search(
-        rho_ab_partial_null: Tensor0[tf32],
-        rho_ab_partial_power: Tensor0[tf32],
-        n: Tensor0[ti64],
-        stats_tensor: Tensor2[tf32, Samples, Stats],
-) -> Tensor1[tf32, Ys]:
-
-    n_float = tf.cast(n, tf.float32)
-
-    log_likelihood_fn_alternative = functools.partial(
-        negative_log_likelihood_from_transformed_params,
-        stats_tensor[None], n,
-    )
-    log_likelihood_fn_null = functools.partial(
-        negative_log_likelihood_from_transformed_params_fixed_ab,
-        stats_tensor[None], n, rho_ab_partial_null,
-    )
-    log_likelihood_fn_power_null = functools.partial(
-        negative_log_likelihood_from_transformed_params_fixed_ab,
-        stats_tensor[None], n, rho_ab_partial_power,
-    )
-
-    # use sample correlations as basis for initial guess
-    correlations_hat = biparcorr.estimate_correlations_safe(stats_tensor[None],
-                                                            n_float)
-    prop_a_hat = tf.reduce_sum(stats_tensor[:, 0], keepdims=True) / n_float
-
-    rho_ab_hat, rho_bc_hat, rho_ac_hat = tf.unstack(correlations_hat, axis=1)
-    rho_ab_partial_hat = ((rho_ab_hat - rho_bc_hat*rho_ac_hat) /
-                          tf.sqrt((1. - tf.square(rho_bc_hat))
-                                  * (1. - tf.square(rho_ac_hat))))
-
-    params_unknown_transformed_init = transform_params(rho_ab_partial_hat,
-                                                       rho_bc_hat,
-                                                       rho_ac_hat,
-                                                       prop_a_hat)
-
-    res_alt = tfp.optimizer.lbfgs_minimize(
-        lambda stats: tfp.math.value_and_gradient(
-            log_likelihood_fn_alternative,
-            stats,
-        ),
-        params_unknown_transformed_init,
-    )
-    lr_alt = -res_alt.objective_value
-
-    res_null = tfp.optimizer.lbfgs_minimize(
-        lambda stats: tfp.math.value_and_gradient(
-            log_likelihood_fn_null,
-            stats,
-        ),
-        params_unknown_transformed_init[:, 1:],
-    )
-    lr_null = -res_null.objective_value
-
-    res_power_null = tfp.optimizer.lbfgs_minimize(
-        lambda stats: tfp.math.value_and_gradient(
-            log_likelihood_fn_power_null,
-            stats,
-        ),
-        params_unknown_transformed_init[:, 1:],
-    )
-    lr_power_null = -res_power_null.objective_value
-
-    return tf.stack([lr_alt[0] - lr_null[0], lr_alt[0] - lr_power_null[0]])
 
 
 @tf.function(jit_compile=True)
@@ -378,8 +341,13 @@ def ps_for_batch(
     n: Tensor1[tf32, Batch],
     rho_ab_partial_power: Tensor1[tf32, Batch],
     batch_size: int,
-    do_l_bfgs: bool,
+    hyperparameter_overrides: Optional[Dict[str, Tensor0]] = None,
 ) -> Tensor2[tf32, Batch, Ys]:
+
+    if hyperparameter_overrides is None:
+        hyperparameters = HYPERPARAMETERS_DEFAULT
+    else:
+        hyperparameters = HYPERPARAMETERS_DEFAULT | hyperparameter_overrides
 
     stats = biparcorr.sampling_distribution_fn_raw(
         rho_ab_partial=rho_ab_partial,
@@ -393,17 +361,11 @@ def ps_for_batch(
     diffs = likelihood_ratios_via_gradient_ascent(stats,
                                                   rho_ab_partial,
                                                   rho_ab_partial_power,
-                                                  n)
-
-    if do_l_bfgs:
-        diff_bfgs = tf.map_fn(
-            functools.partial(likelihood_ratio_via_line_search,
-                              rho_ab_partial, rho_ab_partial_power, n),
-            stats,
-            parallel_iterations=batch_size,
-            fn_output_signature=tf.TensorSpec([2], tf.float32),
-        )
-        diffs = tf.concat([diffs, diff_bfgs], axis=1)
+                                                  rho_bc,
+                                                  rho_ac,
+                                                  prop_a,
+                                                  n,
+                                                  **hyperparameters)
 
     ps_lr = tf.math.igammac(0.5, 0.5 * 2.0*diffs)
 
---
+++ examples/biserial_partial_correlation/biparcorr_step3a_get_reference_distributions_via_bfgs.py
@@ -0,0 +1,159 @@
+import biparcorr_analyse_funcs as biparcorr
+import biparcorr_likelihood_ratio_funcs as lr
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+import functools
+from tqdm import tqdm
+
+# typing
+from neuralcis.common import Batch, Samples, Stats, Ys
+from tensor_annotations.tensorflow import Tensor1, Tensor3
+from tensor_annotations.tensorflow import float32 as tf32
+
+
+def likelihoods_via_line_search(
+        rho_ab_partial_null: Tensor1[tf32, Batch],
+        rho_ab_partial_power: Tensor1[tf32, Batch],
+        rho_bc: Tensor1[tf32, Batch],
+        rho_ac: Tensor1[tf32, Batch],
+        prop_a: Tensor1[tf32, Batch],
+        n: Tensor1[tf32, Batch],
+        stats_tensor: Tensor3[tf32, Batch, Samples, Stats],
+) -> Tensor1[tf32, Ys]:
+
+    # We will even allow it to cheat by knowing the population params!!
+    params_unknown_transformed_init = lr.transform_params(
+        rho_ab_partial_null,
+        rho_bc,
+        rho_ac,
+        prop_a,
+    )
+    rho_ab_partial_null_trans = lr.transform_correlations(rho_ab_partial_null)
+    rho_ab_partial_pow_trans = lr.transform_correlations(rho_ab_partial_power)
+
+    neg_log_likelihood_fn_alternative = functools.partial(
+        lambda *args, **kw:
+            -lr.log_likelihood_from_transformed_params(*args, **kw),
+        stats_tensor, n,
+    )
+    neg_log_likelihood_fn_null = functools.partial(
+        lambda *args, **kw:
+            -lr.log_likelihood_from_transformed_params_fixed_ab(*args, **kw),
+        stats_tensor, n, rho_ab_partial_null_trans,
+    )
+    neg_log_likelihood_fn_power_null = functools.partial(
+        lambda *args, **kw:
+            -lr.log_likelihood_from_transformed_params_fixed_ab(*args, **kw),
+        stats_tensor, n, rho_ab_partial_pow_trans,
+    )
+
+    res_alt = tfp.optimizer.bfgs_minimize(
+        lambda params_unknown_transformed: tfp.math.value_and_gradient(
+            neg_log_likelihood_fn_alternative,
+            params_unknown_transformed,
+        ),
+        params_unknown_transformed_init,
+        stopping_condition=tfp.optimizer.converged_all,
+    )
+    params_unknown_transformed_alt = res_alt.position
+
+    res_alt = tfp.optimizer.bfgs_minimize(
+        lambda params_unknown_transformed: tfp.math.value_and_gradient(
+            neg_log_likelihood_fn_alternative,
+            params_unknown_transformed,
+        ),
+        params_unknown_transformed_alt,
+        stopping_condition=tfp.optimizer.converged_all,
+    )
+    likelihood_alt = -neg_log_likelihood_fn_alternative(
+        res_alt.position,
+        penalise_boundaries=False,
+    )
+
+    res_null = tfp.optimizer.bfgs_minimize(
+        lambda params_unknown_transformed: tfp.math.value_and_gradient(
+            neg_log_likelihood_fn_null,
+            params_unknown_transformed,
+        ),
+        params_unknown_transformed_alt[:, 1:],
+        stopping_condition=tfp.optimizer.converged_all,
+    )
+    likelihood_null = -neg_log_likelihood_fn_null(
+        res_null.position,
+        penalise_boundaries=False,
+    )
+
+    res_power_null = tfp.optimizer.bfgs_minimize(
+        lambda params_unknown_transformed: tfp.math.value_and_gradient(
+            neg_log_likelihood_fn_power_null,
+            params_unknown_transformed,
+        ),
+        params_unknown_transformed_alt[:, 1:],
+        stopping_condition=tfp.optimizer.converged_all,
+    )
+    likelihood_power_null = -neg_log_likelihood_fn_power_null(
+        res_power_null.position,
+        penalise_boundaries=False,
+    )
+
+    num_true_a = tf.reduce_sum(stats_tensor[:, :, 0], axis=1)
+    ill_conditioned = tf.cast((num_true_a == 0.) | (num_true_a == n),
+                              tf.float32)
+
+    likelihoods = tf.stack([likelihood_alt,
+                            likelihood_null,
+                            likelihood_power_null], axis=1)
+
+    results = tf.concat([likelihoods,
+                         ill_conditioned[:, None],
+                         res_alt.position,
+                         res_null.position,
+                         res_power_null.position], axis=1)
+
+    return results
+
+
+@tf.function
+def likelihoods_for_batch(
+        batch_size: int,
+        **params_per_batch_sample: Tensor1[tf32, Batch],
+):
+
+    line_search_args = [
+        params_per_batch_sample[key] for key in [
+            'rho_ab_partial', 'rho_ab_partial_power',
+            'rho_bc', 'rho_ac', 'prop_a', 'n'
+        ]
+    ]
+    rho_ab_partial_power = params_per_batch_sample.pop('rho_ab_partial_power')
+    stats = biparcorr.sampling_distribution_fn_raw(batch_size=batch_size,
+                                                   **params_per_batch_sample)
+    line_search_args += [stats]
+    likelihoods = likelihoods_via_line_search(*line_search_args)
+
+    return likelihoods
+
+
+params = biparcorr.load_params_dict()
+batch_size = 1000
+
+num_param_samples = biparcorr.get_num_param_samples(params)
+target_powers = params.pop('target_power')
+
+likelihoods_list = []
+for param_sample_num in tqdm(range(num_param_samples)):
+    biparcorr.start_first_batch_for_param_sample(param_sample_num)
+    params_repeated = biparcorr.replicate_params(params,
+                                                 param_sample_num,
+                                                 batch_size)
+    this_likelihoods = likelihoods_for_batch(batch_size,
+                                             **params_repeated)
+    likelihoods_list.append(this_likelihoods)
+
+likelihoods = tf.stack(likelihoods_list)
+
+likelihoods_path = biparcorr.convert_relative_path('exact_likelihoods.npy')
+np.save(likelihoods_path, likelihoods.numpy())
