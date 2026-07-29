8fddc6ce0dff33303b65c9f535b82e6e2d9f1d12
Phil Assheton <whatsyouremailaddressagain@gmail.com>
2026-07-29 20:17:35 +0200

    Refactor the bootstrap LR code so that each step is separate from the others

--- examples/biserial_partial_correlation/comparisons/step7_aggregate_and_summarise_bootstrap_LR.py
+++ examples/biserial_partial_correlation/comparisons/step7_aggregate_and_summarise_bootstrap_LR.py
@@ -6,21 +6,12 @@ NUM_PARAM_SAMPLES = 500
 
 
 method_name = "bootstrap_lr"
-summary_dict_bootlr, summary_grids_bootlr = biparcorr.summarise_pvalues_files(
+summary_dict, summary_grids = biparcorr.summarise_pvalues_files(
     method_name=method_name,
     data_type="ps",
     num_params=NUM_PARAM_SAMPLES,
 )
 
-summary_dict_neural, summary_grids_neural = biparcorr.summarise_pvalues_files(
-    method_name="neural",
-    data_type="ps",
-    num_params=NUM_PARAM_SAMPLES,
-    only_first_n_pvalues=500,
-)
-
-params = biparcorr.load_params_dict()
-params = {k:p[0:NUM_PARAM_SAMPLES].numpy() for k, p in params.items()}
-
-summary_dict = summary_dict_bootlr | summary_dict_neural | params
-comp.save_summary_parquet(f"summary_{method_name}", summary_dict)
+summary_name = f"summary_{method_name}"
+comp.save_summary_parquet(summary_name, summary_dict)
+comp.save_summary_numpy(summary_name, summary_grids)
--- examples/biserial_partial_correlation/comparisons/step8_combine_neural_and_bfgs_and_bootstrap_dfs.py
+++ examples/biserial_partial_correlation/comparisons/step8_combine_neural_and_bfgs_and_bootstrap_dfs.py
@@ -3,11 +3,25 @@ from neuralcis import comparisons_funcs as comp
 import pandas as pd
 
 df_neural = pd.read_parquet("summaries/summary_neural.parquet")
+df_neural_500 = pd.read_parquet("summaries/summary_neural_500.parquet")
 df_bfgs = pd.read_parquet("summaries/summary_bfgs.parquet")
+df_bootstrap_lr = pd.read_parquet("summaries/summary_bootstrap_lr.parquet")
 params = pd.DataFrame(biparcorr.load_params_dict())
 
-df = pd.concat([params.reset_index(drop=True),
-                df_neural.reset_index(drop=True),
-                df_bfgs.reset_index(drop=True)], axis=1)
+# The dataframe already restricts us to 500 p-values per parameter but still
+# covers the whole 1000 parameter range.  So trim that to just the first 500
+# parameters
+df_neural_500 = df_neural_500.iloc[0:500, :]
+df_bootstrap_lr = df_bootstrap_lr.iloc[0:500, :]
+params_500 = params.iloc[0:500, :]
 
-comp.save_summary_parquet(f"summary", df)
+df_chisq = pd.concat([params.reset_index(drop=True),
+                      df_neural.reset_index(drop=True),
+                      df_bfgs.reset_index(drop=True)], axis=1)
+
+df_bootstrap_lr = pd.concat([params_500.reset_index(drop=True),
+                             df_neural_500.reset_index(drop=True),
+                             df_bootstrap_lr.reset_index(drop=True)], axis=1)
+
+comp.save_summary_parquet(f"summary_neural_vs_chisq", df_chisq)
+comp.save_summary_parquet(f"summary_neural_vs_bootstrap_lr", df_bootstrap_lr)
