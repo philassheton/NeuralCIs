90955dfc6e4588062c1e9cd83bf5963cfa695db6
Phil Assheton <whatsyouremailaddressagain@gmail.com>
2026-07-29 09:47:52 +0200

    Add first-500-p-values-only summary for comparison to bootstrap LR.

--- examples/biserial_partial_correlation/comparisons/step5_aggregate_and_summarize_neural.py
+++ examples/biserial_partial_correlation/comparisons/step5_aggregate_and_summarize_neural.py
@@ -10,3 +10,15 @@ summary_dict, summary_grids = biparcorr.summarise_pvalues_files(
 summary_name = f"summary_{method_name}"
 comp.save_summary_parquet(summary_name, summary_dict)
 comp.save_summary_numpy(summary_name, summary_grids)
+
+
+# Repeat but for only first 500 p-values, for comparison with bootstrap LR
+summary_dict_500, summary_grids_500 = biparcorr.summarise_pvalues_files(
+    method_name=method_name,
+    data_type="ps",
+    only_first_n_pvalues=500,
+)
+
+summary_name_500 = f"summary_{method_name}_500"
+comp.save_summary_parquet(summary_name_500, summary_dict_500)
+comp.save_summary_numpy(summary_name_500, summary_grids_500)
