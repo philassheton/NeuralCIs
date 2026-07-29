33a27a2d89e653f4287e50f02fe984f222dfc990
Phil Assheton <whatsyouremailaddressagain@gmail.com>
2026-07-29 17:47:41 +0200

    Make sure different training profiles are available to end user.

--- neuralcis/__init__.py
+++ neuralcis/__init__.py
@@ -1,3 +1,4 @@
 from .neuralcis import NeuralCIs
+from .common import INFERENCE, TESTING, FULL
 from .variables import Stat, StatCanonical, Param, KnownParam, Interest
 from .variables import Location, Scale, PositiveCount, Correlation, Proportion
